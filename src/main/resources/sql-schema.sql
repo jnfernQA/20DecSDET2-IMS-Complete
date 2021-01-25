a42601088461ef422ced8fd36b554b8a01c1c523
jnfernQA <JFernandes@qa.com>
2021-01-25 17:34:01 +0000

    Updated Schema

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) NOT NULL,
+    `item_price` DOUBLE(5,2) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `cust_id` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`cust_id`) REFERENCES `ims`.`customers`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orderItems` (
+    `order_id` INT(11) NOT NULL,
+    `item_id` INT(11) NOT NULL,
+    FOREIGN KEY (`order_id`) REFERENCES `ims`.`orders`(`id`),
+    FOREIGN KEY (`item_id`) REFERENCES `ims`.`items`(`id`)
+);
