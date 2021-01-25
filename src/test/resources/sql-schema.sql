ac4d5abfb99b01dfec6141163660577eadc6b17f
jnfernQA <JFernandes@qa.com>
2021-01-25 14:57:13 +0000

    Added sql schema for items

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS `customers` (
     PRIMARY KEY (`id`)
 );
 
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) NOT NULL,
+    `item_price` DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY (`id`)
+);
